33935c8f46830eca7379039161fbe2607739b769
Álvaro Castellano Vela <alvaro.castellano.vela@gmail.com>
2021-06-21 23:02:19 +0200

    Define StoraManager connector

---
+++ storage/storage.go
@@ -0,0 +1,28 @@
+package storage
+
+import (
+	"encoding/json"
+	"errors"
+	commontypes "github.com/a-castellano/music-manager-common-types/types"
+	"net/http"
+
+	"bytes"
+)
+
+func sendInfoToStorageManager(client http.Client, storageService string, job commontypes.Job) error {
+
+	jsonJob, _ := json.Marshal(job)
+	url := "http://" + storageService
+	resp, err := client.Post(url, "application/json", bytes.NewBuffer(jsonJob))
+
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != 200 {
+		// We must include a reason for that error
+		return errors.New("Failed to update status.")
+	}
+
+	return nil
+}
