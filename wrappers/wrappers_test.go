c747092415c4e8a51fd6f8888eb3eb11cde856c0
Álvaro Castellano Vela <alvaro.castellano.vela@gmail.com>
2021-07-06 23:00:54 +0200

    send channel info inside go func

--- wrappers/wrappers_test.go
+++ wrappers/wrappers_test.go
@@ -3,13 +3,14 @@
 package wrappers
 
 import (
+	"bytes"
+	"io/ioutil"
 	"log"
-	"strconv"
+	"net/http"
 	"testing"
 
 	commontypes "github.com/a-castellano/music-manager-common-types/types"
 	"github.com/a-castellano/music-manager-job-router/config"
-	"github.com/streadway/amqp"
 )
 
 func failOnError(err error, msg string) {
@@ -18,7 +19,16 @@ func failOnError(err error, msg string) {
 	}
 }
 
-func TestReceive(t *testing.T) {
+type RoundTripperMock struct {
+	Response *http.Response
+	RespErr  error
+}
+
+func (rtm *RoundTripperMock) RoundTrip(*http.Request) (*http.Response, error) {
+	return rtm.Response, rtm.RespErr
+}
+
+func TestReceiveDie(t *testing.T) {
 
 	var testConfig config.Config
 
@@ -37,84 +47,122 @@ func TestReceive(t *testing.T) {
 	job.ID = "dassa111a"
 	job.Status = true
 	job.Finished = false
-	job.Type = commontypes.ArtistInfoRetrieval
+	job.Type = commontypes.Die
 	job.LastOrigin = "JobRouter"
 
 	wrapperChannel := make(chan commontypes.Job)
 
-	wrapperChannel <- job
+	go func() { wrapperChannel <- job }()
 
-	connectionString := "amqp://" + testConfig.Server.User + ":" + testConfig.Server.Password + "@" + testConfig.Server.Host + ":" + strconv.Itoa(testConfig.Server.Port) + "/"
-	conn, err := amqp.Dial(connectionString)
+	client := http.Client{Transport: &RoundTripperMock{Response: &http.Response{StatusCode: 200, Body: ioutil.NopCloser(bytes.NewBufferString(`
+	not html code
+		`))}}}
 
-	if err != nil {
-		failOnError(err, "Failed to stablish connection with RabbitMQ")
-	}
-	defer conn.Close()
-
-	firstWrapperCh, err := conn.Channel()
-	defer firstWrapperCh.Close()
+	err := RouteJobs(testConfig, wrapperChannel, client)
 
 	if err != nil {
-		failOnError(err, "Failed to open first Wrapper RabbitMQ channel")
+		t.Errorf("TestReceiveDie should end without errors.")
 	}
 
-	firstWrapperQ, err := firstWrapperCh.QueueDeclare(
-		testConfig.Wrappers[0].Name,
-		true,  // Durable
-		false, // DeleteWhenUnused
-		false, // Exclusive
-		false, // NoWait
-		nil,   // arguments
-	)
-
-	if err != nil {
-		failOnError(err, "Failed to declare firstWrapper queue.")
-	}
-
-	err = firstWrapperCh.Qos(
-		1,     // prefetch count
-		0,     // prefetch size
-		false, // global
-	)
-
-	if err != nil {
-		failOnError(err, "Failed to set firstWrapper QoS.")
-	}
-
-	jobsToProcess, err := firstWrapperCh.Consume(
-		firstWrapperQ.Name,
-		"",    // consumer
-		false, // auto-ack
-		false, // exclusive
-		false, // no-local
-		false, // no-wait
-		nil,   // args
-	)
-
-	if err != nil {
-		failOnError(err, "Failed to register a consumer")
-	}
-
-	forever := make(chan bool)
-	var receivedData []byte
-
-	go func() {
-		for d := range jobsToProcess {
-
-			receivedData = d.Body
-			d.Ack(true)
-			forever <- false
-		}
-	}()
-
-	<-forever
-	decodedJob, decodedJobErr := commontypes.DecodeJob(receivedData)
-
-	if decodedJobErr != nil {
-		t.Errorf("TestReceive shouldn't fail decoding job.")
-	}
-	if decodedJob.ID != job.ID {
-		t.Errorf("job and decodedJob should have the same ID.")
-	}
 }
+
+//func TestReceive(t *testing.T) {
+//
+//	var testConfig config.Config
+//
+//	testConfig.Server.Host = "rabbitmq"
+//	testConfig.Server.Port = 5672
+//	testConfig.Server.User = "guest"
+//	testConfig.Server.Password = "guest"
+//	testConfig.JobManager.Name = "JobManager"
+//
+//	firstwrapper := config.Queue{Name: "first"}
+//
+//	testConfig.Wrappers = append(testConfig.Wrappers, firstwrapper)
+//
+//	var job commontypes.Job
+//
+//	job.ID = "dassa111a"
+//	job.Status = true
+//	job.Finished = false
+//	job.Type = commontypes.Die
+//	job.LastOrigin = "JobRouter"
+//
+//	wrapperChannel := make(chan commontypes.Job)
+//
+//	wrapperChannel <- job
+//
+//	connectionString := "amqp://" + testConfig.Server.User + ":" + testConfig.Server.Password + "@" + testConfig.Server.Host + ":" + strconv.Itoa(testConfig.Server.Port) + "/"
+//	conn, err := amqp.Dial(connectionString)
+//
+//	if err != nil {
+//		failOnError(err, "Failed to stablish connection with RabbitMQ")
+//	}
+//	defer conn.Close()
+//
+//	firstWrapperCh, err := conn.Channel()
+//	defer firstWrapperCh.Close()
+//
+//	if err != nil {
+//		failOnError(err, "Failed to open first Wrapper RabbitMQ channel")
+//	}
+//
+//	firstWrapperQ, err := firstWrapperCh.QueueDeclare(
+//		testConfig.Wrappers[0].Name,
+//		true,  // Durable
+//		false, // DeleteWhenUnused
+//		false, // Exclusive
+//		false, // NoWait
+//		nil,   // arguments
+//	)
+//
+//	if err != nil {
+//		failOnError(err, "Failed to declare firstWrapper queue.")
+//	}
+//
+//	err = firstWrapperCh.Qos(
+//		1,     // prefetch count
+//		0,     // prefetch size
+//		false, // global
+//	)
+//
+//	if err != nil {
+//		failOnError(err, "Failed to set firstWrapper QoS.")
+//	}
+//
+//	jobsToProcess, err := firstWrapperCh.Consume(
+//		firstWrapperQ.Name,
+//		"",    // consumer
+//		false, // auto-ack
+//		false, // exclusive
+//		false, // no-local
+//		false, // no-wait
+//		nil,   // args
+//	)
+//
+//	if err != nil {
+//		failOnError(err, "Failed to register a consumer")
+//	}
+//
+//	forever := make(chan bool)
+//	var receivedData []byte
+//
+//	go func() {
+//		for d := range jobsToProcess {
+//
+//			receivedData = d.Body
+//			d.Ack(true)
+//			forever <- false
+//		}
+//	}()
+//
+//	<-forever
+//	decodedJob, decodedJobErr := commontypes.DecodeJob(receivedData)
+//
+//	if decodedJobErr != nil {
+//		t.Errorf("TestReceive shouldn't fail decoding job.")
+//	}
+//	if decodedJob.ID != job.ID {
+//		t.Errorf("job and decodedJob should have the same ID.")
+//	}
+//}
