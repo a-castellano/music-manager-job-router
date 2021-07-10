7b4521affc2c85e860b0f4ad842cf39b02dbee51
Álvaro Castellano Vela <alvaro.castellano.vela@gmail.com>
2021-07-10 17:45:02 +0200

    Test Receive Failed Job One More Wrapper Job And Die

--- wrappers/wrappers_test.go
+++ wrappers/wrappers_test.go
@@ -7,10 +7,12 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 	"testing"
 
 	commontypes "github.com/a-castellano/music-manager-common-types/types"
 	"github.com/a-castellano/music-manager-job-router/config"
+	"github.com/streadway/amqp"
 )
 
 func failOnError(err error, msg string) {
@@ -165,103 +167,122 @@ func TestReceiveFailedJobNoMoreWrappersJobAndDie(t *testing.T) {
 
 }
 
-//func TestReceive(t *testing.T) {
-//
-//	var testConfig config.Config
-//
-//	testConfig.Server.Host = "rabbitmq"
-//	testConfig.Server.Port = 5672
-//	testConfig.Server.User = "guest"
-//	testConfig.Server.Password = "guest"
-//	testConfig.JobManager.Name = "JobManager"
-//
-//	firstwrapper := config.Queue{Name: "first"}
-//
-//	testConfig.Wrappers = append(testConfig.Wrappers, firstwrapper)
-//
-//	var job commontypes.Job
-//
-//	job.ID = "dassa111a"
-//	job.Status = true
-//	job.Finished = false
-//	job.Type = commontypes.Die
-//	job.LastOrigin = "JobRouter"
-//
-//	wrapperChannel := make(chan commontypes.Job)
-//
-//	wrapperChannel <- job
-//
-//	connectionString := "amqp://" + testConfig.Server.User + ":" + testConfig.Server.Password + "@" + testConfig.Server.Host + ":" + strconv.Itoa(testConfig.Server.Port) + "/"
-//	conn, err := amqp.Dial(connectionString)
-//
-//	if err != nil {
-//		failOnError(err, "Failed to stablish connection with RabbitMQ")
-//	}
-//	defer conn.Close()
-//
-//	firstWrapperCh, err := conn.Channel()
-//	defer firstWrapperCh.Close()
-//
-//	if err != nil {
-//		failOnError(err, "Failed to open first Wrapper RabbitMQ channel")
-//	}
-//
-//	firstWrapperQ, err := firstWrapperCh.QueueDeclare(
-//		testConfig.Wrappers[0].Name,
-//		true,  // Durable
-//		false, // DeleteWhenUnused
-//		false, // Exclusive
-//		false, // NoWait
-//		nil,   // arguments
-//	)
-//
-//	if err != nil {
-//		failOnError(err, "Failed to declare firstWrapper queue.")
-//	}
-//
-//	err = firstWrapperCh.Qos(
-//		1,     // prefetch count
-//		0,     // prefetch size
-//		false, // global
-//	)
-//
-//	if err != nil {
-//		failOnError(err, "Failed to set firstWrapper QoS.")
-//	}
-//
-//	jobsToProcess, err := firstWrapperCh.Consume(
-//		firstWrapperQ.Name,
-//		"",    // consumer
-//		false, // auto-ack
-//		false, // exclusive
-//		false, // no-local
-//		false, // no-wait
-//		nil,   // args
-//	)
-//
-//	if err != nil {
-//		failOnError(err, "Failed to register a consumer")
-//	}
-//
-//	forever := make(chan bool)
-//	var receivedData []byte
-//
-//	go func() {
-//		for d := range jobsToProcess {
-//
-//			receivedData = d.Body
-//			d.Ack(true)
-//			forever <- false
-//		}
-//	}()
-//
-//	<-forever
-//	decodedJob, decodedJobErr := commontypes.DecodeJob(receivedData)
-//
-//	if decodedJobErr != nil {
-//		t.Errorf("TestReceive shouldn't fail decoding job.")
-//	}
-//	if decodedJob.ID != job.ID {
-//		t.Errorf("job and decodedJob should have the same ID.")
-//	}
-//}
+func TestReceiveFailedJobOneMoreWrapperJobAndDie(t *testing.T) {
+
+	client := http.Client{Transport: &RoundTripperMock{Response: &http.Response{StatusCode: 200, Body: ioutil.NopCloser(bytes.NewBufferString(`
+	not html code
+		`))}}}
+
+	var testConfig config.Config
+
+	testConfig.Server.Host = "rabbitmq"
+	testConfig.Server.Port = 5672
+	testConfig.Server.User = "guest"
+	testConfig.Server.Password = "guest"
+	testConfig.JobManager.Name = "JobManager"
+
+	firstwrapper := config.Queue{Name: "first"}
+	secondwrapper := config.Queue{Name: "second"}
+
+	testConfig.Wrappers = append(testConfig.Wrappers, firstwrapper)
+	testConfig.Wrappers = append(testConfig.Wrappers, secondwrapper)
+
+	var dieJob commontypes.Job
+	var unfinishedJob commontypes.Job
+
+	dieJob.ID = "TestReceiveFailedJobOneMoreWrapperJobAndDie"
+	dieJob.Status = true
+	dieJob.Finished = false
+	dieJob.Type = commontypes.Die
+	dieJob.LastOrigin = "JobRouter"
+	dieJob.RequiredOrigin = "JobRouter"
+
+	unfinishedJob.ID = "TestReceiveFailedJobOneMoreWrapperJobAndDie"
+	unfinishedJob.Status = false
+	unfinishedJob.Finished = false
+	unfinishedJob.Type = commontypes.ArtistInfoRetrieval
+	unfinishedJob.LastOrigin = "first"
+
+	wrapperChannel := make(chan commontypes.Job)
+
+	go func() {
+		wrapperChannel <- unfinishedJob
+		wrapperChannel <- dieJob
+	}()
+
+	err := RouteJobs(testConfig, wrapperChannel, client)
+
+	connectionString := "amqp://" + testConfig.Server.User + ":" + testConfig.Server.Password + "@" + testConfig.Server.Host + ":" + strconv.Itoa(testConfig.Server.Port) + "/"
+	conn, err := amqp.Dial(connectionString)
+
+	if err != nil {
+		failOnError(err, "Failed to stablish connection with RabbitMQ")
+	}
+	defer conn.Close()
+
+	secondWrapperCh, err := conn.Channel()
+	defer secondWrapperCh.Close()
+
+	if err != nil {
+		failOnError(err, "Failed to open first Wrapper RabbitMQ channel")
+	}
+
+	secondWrapperQ, err := secondWrapperCh.QueueDeclare(
+		testConfig.Wrappers[1].Name,
+		true,  // Durable
+		false, // DeleteWhenUnused
+		false, // Exclusive
+		false, // NoWait
+		nil,   // arguments
+	)
+
+	if err != nil {
+		failOnError(err, "Failed to declare secondWrapper queue.")
+	}
+
+	err = secondWrapperCh.Qos(
+		1,     // prefetch count
+		0,     // prefetch size
+		false, // global
+	)
+
+	if err != nil {
+		failOnError(err, "Failed to set firstWrapper QoS.")
+	}
+
+	jobsToProcess, err := secondWrapperCh.Consume(
+		secondWrapperQ.Name,
+		"",    // consumer
+		false, // auto-ack
+		false, // exclusive
+		false, // no-local
+		false, // no-wait
+		nil,   // args
+	)
+
+	if err != nil {
+		failOnError(err, "Failed to register a consumer")
+	}
+
+	forever := make(chan bool)
+	var receivedData []byte
+
+	go func() {
+		for d := range jobsToProcess {
+
+			receivedData = d.Body
+			d.Ack(true)
+			forever <- false
+		}
+	}()
+
+	<-forever
+	decodedJob, decodedJobErr := commontypes.DecodeJob(receivedData)
+
+	if decodedJobErr != nil {
+		t.Errorf("TestReceive shouldn't fail decoding job.")
+	}
+	if decodedJob.ID != unfinishedJob.ID {
+		t.Errorf("job and decodedJob should have the same ID.")
+	}
+}
