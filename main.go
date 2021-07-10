3fe3ba02cf172bb171d4073ff0bf25331a08b458
Álvaro Castellano Vela <alvaro.castellano.vela@gmail.com>
2021-07-10 23:51:22 +0200

    Write main

--- main.go
+++ main.go
@@ -2,8 +2,40 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	commontypes "github.com/a-castellano/music-manager-common-types/types"
+	"github.com/a-castellano/music-manager-job-router/config"
+	"github.com/a-castellano/music-manager-job-router/manager"
+	"github.com/a-castellano/music-manager-job-router/wrappers"
 )
 
 func main() {
-	fmt.Println("Hello World")
+
+	client := http.Client{
+		Timeout: time.Second * 5, // Maximum of 5 secs
+	}
+
+	log.Println("Reading config.")
+
+	jobRouterConfig, err := config.ReadConfig()
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	} else {
+		log.Println("Config readed successfully.")
+
+		wrapperChannel := make(chan commontypes.Job)
+		go manager.ReadJobManagerJobs(jobRouterConfig, wrapperChannel)
+		jobRouterError := wrappers.RouteJobs(jobRouterConfig, wrapperChannel, client)
+
+		if jobRouterError != nil {
+			fmt.Println(jobRouterError)
+		}
+	}
+
 }
